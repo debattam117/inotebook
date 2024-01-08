dfe59bff964282634174b9fe12c1164ca0ef2545
Debattam Bhattacharjee <74034972+debattam117@users.noreply.github.com>
2024-01-08 13:03:12 +0530

    Added express validator in auth.js
    Express validator is a package.
    
    using it u can validate the fields before storing it to db

--- mynotestodo/backend/Routes/auth.js
+++ mynotestodo/backend/Routes/auth.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const User=require('../models/User.js');
+const { body, validationResult } = require('express-validator');
 
 //create a user using post 
-router. post ('/', (req, res) => {
-    
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+router. post ('/',[
 
-    })
+body('name').isLength({min:3}),
+body('email').isEmail(),
+body('password').isLength({min:5}),
+
+] , async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const newUser = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        });
+        res.json(newUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
     
     
 module.exports = router
@@ -20,7 +37,15 @@ module.exports = router
 
 
 
-
+// const result = validationResult(req);
+// if (result.isEmpty()) {
+//     return res.status(400).json({errors:errors.array()});
+//   }
+//   User.create({
+//     name: req.body.name,
+//     password: req.body.password,
+//     email: req.body.email
+// }).then(user => res.json(user));
 
 
 
